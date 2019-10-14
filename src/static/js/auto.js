fdf76e180526677d4282095a882fc81395ef4519
Krateng <git.noreply@krateng.ch>
2019-10-14 16:35:38 +0200

    Enabled play / pause via space key

--- src/static/js/auto.js
+++ src/static/js/auto.js
@@ -1,13 +1,14 @@
 // functions that are automatically assined based on html attributes
 
 SMOOTH_UPDATE = true;
-
-
-
 var update_delay = SMOOTH_UPDATE ? 40 : 500;
 
+
+
 document.addEventListener('DOMContentLoaded', function() {
-	elements = document.getElementsByClassName("seekable");
+
+	/* SEEKABLE */
+	var elements = document.getElementsByClassName("seekable");
 	for (var i=0;i<elements.length;i++) {
 		callback = elements[i].getAttribute("data-seekcallback");
 		elements[i].addEventListener("click",function(evt) {
@@ -18,8 +19,9 @@ document.addEventListener('DOMContentLoaded', function() {
 		})
 	}
 
-	elements2 = document.getElementsByClassName("update");
-	functions = []
+	/* AUTO UPDATE */
+	var elements2 = document.getElementsByClassName("update");
+	var functions = []
 	for (var i=0;i<elements2.length;i++) {
 		updatefunc = elements2[i].getAttribute("data-updatefrom");
 		functions.push([elements2[i],updatefunc])
@@ -34,5 +36,22 @@ document.addEventListener('DOMContentLoaded', function() {
 		setTimeout(supervisor,update_delay);
 	}
 	supervisor();
+
+}, false);
+
+
+
+/* HOTKEYS */
+
+document.addEventListener('keyup', function(evt) {
+
+	var elements = document.querySelectorAll('[data-hotkey]');
+	for (let e of elements) {
+		if (e.getAttribute("data-hotkey") == evt.code) {
+			evt.preventDefault();
+			e.onclick();
+			break;
+		}
+	}
 
 }, false);
